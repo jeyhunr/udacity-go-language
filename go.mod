b8dbc83b5fcf5756743f4efbae35ef25a9030044
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2024-06-10 21:19:31 +0200

    add: create go.mod and Customer strunct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeyhunr/udacity-go-language
+
+go 1.21.1
--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type Customer struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Role      string `json:"role"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Contacted bool   `json:"contacted"`
+}
+
 func main() {
 	fmt.Println("Hello world!")
 }
